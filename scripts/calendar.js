559db122322420998bcd0b21833d7074ba91eef1
LeGrandSolo <snowblybg@gmail.com>
2022-07-09 18:39:48 +0300

    Implemented not working input feature for dates

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,8 +1,8 @@
-const calendarElem = document.getElementById("calendar");
 const datesElem = document.querySelector(".dates");
 const div = document.createElement("div");
 const pMonth = document.createElement("p");
 const pYear = document.createElement("p");
+const popup = document.querySelector("#popup");
 const currDate = new Date();
 let monthUsrIsOn = currDate.getMonth();
 let yearUsrIsOn = currDate.getFullYear();
@@ -20,7 +20,7 @@ const months = [
   "November",
   "December",
 ];
-datesElem.addEventListener("click", showPopupOnDate);
+const dateData = {};
 document.getElementById("prev").addEventListener("click", changeToPrevMonth);
 document.getElementById("next").addEventListener("click", changeToNextMonth);
 generateMonthAndYear();
@@ -47,12 +47,13 @@ function generateDates() {
       i <= hightestDateOfPrevMonth;
       i++
     ) {
-      createAndAppendLiWithDateNum(i, false, "not-curr-month");
+      createAndAppendLiWithDateNum(i, false, ["not-curr-month", "prev-month"]);
     }
   }
   let numOfDaysInCurrMonth = getNumberOfDaysOfMonth(yearUsrIsOn, monthUsrIsOn);
   for (let i = 1; i <= numOfDaysInCurrMonth; i++) {
-    createAndAppendLiWithDateNum(i, true, "curr-month");
+    dateData[i] = document.createElement("ul");
+    createAndAppendLiWithDateNum(i, true, ["curr-month"]);
   }
   const weekDayOfLastDateOfMonth = getWeekDay(
     yearUsrIsOn,
@@ -62,23 +63,28 @@ function generateDates() {
   if (weekDayOfLastDateOfMonth !== 0) {
     let addDate = 1;
     for (let i = 7 - (7 - weekDayOfLastDateOfMonth); i < 7; i++) {
-      createAndAppendLiWithDateNum(addDate, false, "not-curr-month");
+      createAndAppendLiWithDateNum(addDate, false, [
+        "not-curr-month",
+        "next-month",
+      ]);
       addDate++;
     }
   }
 }
-function createAndAppendLiWithDateNum(num, isCurrMonth, liClassName) {
+function createAndAppendLiWithDateNum(num, isCurrMonth, liClassArr) {
   const li = document.createElement("li");
   li.textContent = num;
   if (isCurrMonth) {
     if (num === currDate.getDate()) {
       li.id = "active";
+      showPopupOnSelectedDate(num);
     }
   }
-  li.className = liClassName;
+  for (const className of liClassArr) {
+    li.classList.add(className);
+  }
   datesElem.appendChild(li);
 }
-
 function generateMonthAndYear() {
   pMonth.textContent = months[monthUsrIsOn];
   pYear.textContent = currDate.getFullYear();
@@ -92,14 +98,60 @@ function generateMonthAndYear() {
   document.getElementById("month-header").appendChild(div);
 }
 function selectActiveDate() {
-  const dates = document.querySelector(".dates");
-  dates.addEventListener("click", (e) => {
+  datesElem.addEventListener("click", (e) => {
+    const prevActive = document.getElementById("active");
     if (e.target.nodeName === "LI") {
-      const prevActive = document.getElementById("active");
-      prevActive.id = "";
-      e.target.id = "active";
+      if (e.target.className === "curr-month") {
+        checkPrevActive(e);
+      } else if (e.target.className === "prev-month") {
+        changeToPrevMonth();
+        checkPrevActive(e);
+      } else if (e.target.className === "next-month") {
+        changeToNextMonth();
+        checkPrevActive(e);
+      }
+      function checkPrevActive(e) {
+        if (prevActive !== e.target) {
+          prevActive.id = "";
+          e.target.id = "active";
+          showPopupOnSelectedDate(e);
+        }
+      }
     }
   });
+}
+//if initializing the first arg is num not event
+function showPopupOnSelectedDate(e) {
+  const input = document.createElement("input");
+  const btn = document.createElement("button");
+  btn.style.width = "1em";
+  btn.style.height = "1em";
+  while (popup.childNodes[2]) {
+    popup.childNodes[2].remove();
+  }
+  let ul;
+  if (typeof e === 'number') {
+    ul = dateData[e];
+  }else{
+    ul = dateData[e.target.textContent];
+  }
+  btn.addEventListener("click", () => {
+    const li = document.createElement("li");
+    li.textContent = input.value;
+    li.style.display = "block";
+    let dateDataKey;
+    if (typeof e === 'number') {
+      dateDataKey = e;
+    } else {
+      dateDataKey = e.target.textContente;
+    }
+    ul = dateData[dateDataKey];
+    ul.appendChild(li);
+    dateData[dateData] = ul;
+  });
+  popup.append(input, btn);
+  popup.appendChild(ul);
+  popup.style.display = "block";
 }
 function changeToPrevMonth() {
   if (monthUsrIsOn === 0) {
@@ -126,11 +178,4 @@ function getNumberOfDaysOfMonth(year, month) {
 }
 function getWeekDay(year, month, day) {
   return new Date(year, month, day).getDay();
-}
-function showPopupOnDate(e) {
-  let popup = document.querySelector("#popup");
-  popup.style.display = "block";
-  if (e.target.nodeName === "LI") {
-    popup.textContent = e.target.textContent;
-  }
 }
