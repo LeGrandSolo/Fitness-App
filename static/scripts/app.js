760507a1db3bad707a9b96a96094c09d738c375d
LeGrandSolo <snowblybg@gmail.com>
2022-08-13 18:23:30 +0300

    Test1

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,25 +1,24 @@
-/* import { render, page, html, styleMap } from "./api/lib.js";
+import { render, page} from "./api/lib.js";
 import { getUserData } from "./api/util.js";
 import { showCalendar } from "./calendarView/calendarMain.js";
 import { showExerciseView } from "./exerciseView/excerciseMain.js";
 import { showHome } from "./home/home.js";
 import { layoutTemplate } from "./layout/layout.js";
 import { onLogout, showLogin } from "./users/login.js";
-import { showRegister } from "./users/register.js"; */
+import { showRegister } from "./users/register.js";
 
 const body = document.querySelector("body");
 
-/* page(decorateCtx);
-page("/index.html", "/");
+page(decorateCtx);
+page("/static/index.html", "/");
 page("/my-workouts/:month", showCalendar);
 page("/", showHome);
 page("/login", showLogin);
 page("/register", showRegister);
 page("/excercises", showExerciseView); 
 page.start();
- */
-console.log('Hello');
-/* function renderMain(ctx, template) {
+
+function renderMain(ctx, template) {
   render(layoutTemplate(ctx, template), body);
 }
 
@@ -28,4 +27,4 @@ function decorateCtx(ctx, next) {
   ctx.userData = getUserData();
   ctx.render = renderMain.bind(null, ctx);
   next();
-} */
+}
